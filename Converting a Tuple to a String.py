b7d33c57e67756268d3a66a3a7288f1f983b28a9
srinidhi210404-dev <srinidhi210404@gmail.com>
2026-04-20 11:48:20 +0530

    Create  Converting a Tuple to a String.py

---
+++ Converting a Tuple to a String.py
@@ -0,0 +1,6 @@
+a_tuple = ('Sarmi','Sweety')
+print"The given tuple is",(a_tuple)
+b = " ".join(a_tuple)
+print"The converted string is",(b)
+[23bee022@mepcolinux ~]$python ex6a.py
+The given tuple is ('Sarmi', 'Sweety')
